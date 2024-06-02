6668e052f232249a1d11aa27247bd14e568b3fb3
artkor14152 <artkor14152@yandex.ru>
2024-06-02 11:31:38 +0300

    fix var 19.py

--- var19.py
+++ var19.py
@@ -1,32 +1,34 @@
 import streamlit as st
 import matplotlib.pyplot as plt
 
-def getSurvivedCount(lines,option):
+
+def getSurvivedCount(lines, option):
     notSurvivedC = 0
     notSurvivedQ = 0
     notSurvivedS = 0
     for line in lines:
         data = line.split(",")
         sex = data[5]
-        port=data[12].strip()
+        port = data[12].strip()
         if data[6] != "":
             age = float(data[6])
-        if sex == "male" and age >option:
+        if sex == "male" and age > option:
             if port == "C":
-                notSurvivedC+=1
+                notSurvivedC += 1
             if port == "Q":
-                notSurvivedQ+=1
+                notSurvivedQ += 1
             if port == "S":
-                notSurvivedS+=1
+                notSurvivedS += 1
     return notSurvivedC, notSurvivedQ, notSurvivedS
+
 
 def var19():
     st.title("Подсчитать количество погибших мужчин старше указанного возраста по каждому пункту посадки")
     with open("data.csv") as f:
         next(f)
-        option=st.slider("Выберите возрастной порог", min_value=1, max_value=100)
-        notSurvivedC, notSurvivedQ, notSurvivedS = getSurvivedCount(f.readlines(),option)
-    table = st.table({"Порт": ["Шербур", "Квинстаун", "Саутгемптон"], "Погибших": [notSurvivedC, notSurvivedQ, notSurvivedS]})
+        option = st.slider("Выберите возрастной порог", min_value=1, max_value=100)
+        notSurvivedC, notSurvivedQ, notSurvivedS = getSurvivedCount(f.readlines(), option)
+    st.table({"Порт": ["Шербур", "Квинстаун", "Саутгемптон"], "Погибших": [notSurvivedC, notSurvivedQ, notSurvivedS]})
     fig = plt.figure(figsize=(10, 5))
     plt.xlabel("Порт")
     plt.ylabel("Количество")
