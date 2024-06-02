8b7c5b8277ccadb7995ac4a00b41ad2427e828c1
artkor14152 <artkor14152@yandex.ru>
2024-06-02 11:48:49 +0300

    fix var 2.py

--- var2.py
+++ var2.py
@@ -1,36 +1,40 @@
 import streamlit as st
 import matplotlib.pyplot as plt
+
+
 def getinfo(lines, opt):
-    survived=0
-    notSurvived=0
+    survived = 0
+    notSurvived = 0
     for line in lines:
         data = line.split(",")
         isSurvived = data[1]
-        port=data[12].strip()
-        if port=="C" and opt=="Шербур":
-            if isSurvived=="1":
-                survived+=1
+        port = data[12].strip()
+        if port == "C" and opt == "Шербур":
+            if isSurvived == "1":
+                survived += 1
             else:
-                notSurvived+=1
-        if port=="Q" and opt=="Квинстоун":
-            if isSurvived=="1":
-                survived+=1
+                notSurvived += 1
+        if port == "Q" and opt == "Квинстоун":
+            if isSurvived == "1":
+                survived += 1
             else:
-                notSurvived+=1
-        if port=="S" and opt=="Саутгемптон":
-            if isSurvived=="1":
-                survived+=1
+                notSurvived += 1
+        if port == "S" and opt == "Саутгемптон":
+            if isSurvived == "1":
+                survived += 1
             else:
-                notSurvived+=1
+                notSurvived += 1
     return survived, notSurvived
+
+
 def var2():
-    st.title ('Подсчитать число спасенных и погибших для указанного пункта посадки.')
-    option=st.selectbox("Выбирете пункт посадки ", ["Шербур", "Квинстоун", "Саутгемптон"])
+    st.title('Подсчитать число спасенных и погибших для указанного пункта посадки.')
+    option = st.selectbox("Выбирете пункт посадки ", ["Шербур", "Квинстоун", "Саутгемптон"])
     with open("data.csv") as f:
         next(f)
-        survived, notSurvived = getinfo(f.readlines(),option)
-        if option=="Шербур":
-            table = st.table({"Спасённых":survived, "Погибших":notSurvived})
+        survived, notSurvived = getinfo(f.readlines(), option)
+        if option == "Шербур":
+            st.table({"Спасённых": survived, "Погибших": notSurvived})
             fig = plt.figure(figsize=(10, 5))
             plt.xlabel("Значение поля Survived")
             plt.ylabel("Количество")
@@ -38,7 +42,7 @@ def var2():
             plt.bar(["Спасённых", "Погибших"], [survived, notSurvived])
             st.pyplot(fig)
         elif option=="Квинстоун":
-            table = st.table({"Спасённых":survived, "Погибших":notSurvived})
+            st.table({"Спасённых": survived, "Погибших": notSurvived})
             fig = plt.figure(figsize=(10, 5))
             plt.xlabel("Значение поля Survived")
             plt.ylabel("Количество")
@@ -46,10 +50,11 @@ def var2():
             plt.bar(["Спасённых", "Погибших"], [survived, notSurvived])
             st.pyplot(fig)
         elif option=="Саутгемптон":
-            table = st.table({"Спасённых":survived, "Погибших":notSurvived})
+            table = st.table({"Спасённых": survived, "Погибших": notSurvived})
             fig = plt.figure(figsize=(10, 5))
             plt.xlabel("Значение поля Survived")
             plt.ylabel("Количество")
             plt.title("Количество спасённых и погибших в Саутгемптоне")
             plt.bar(["Спасённых", "Погибших"], [survived, notSurvived])
             st.pyplot(fig)
+            
